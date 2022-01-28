b03c5cc069f48b1df3ea419da890eeff7ed47bd9
NobuYoshi928 <softbank.nttdocomo@gmail.com>
2022-01-29 04:27:25 +0900

    cicd.yml修正

--- app/sample_app.py
+++ app/sample_app.py
@@ -10,7 +10,7 @@ conn = pymysql.connect(
     port=int(os.environ["MYSQL_PORT"]),
     user=os.environ["MYSQL_USER"],
     password=os.environ["MYSQL_PASSWORD"],
-    db=os.environ["MYSQL_DATABASE"],
+    db=os.environ["MYSQL_DB"],
     charset="utf8",
     cursorclass=pymysql.cursors.DictCursor,
 )
