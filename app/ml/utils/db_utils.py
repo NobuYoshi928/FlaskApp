c2f425cc548a693ad2c88cd38635f9370a95d2a7
NobuYoshi928 <softbank.nttdocomo@gmail.com>
2022-02-12 17:30:18 +0900

    deploy test

--- app/ml/utils/db_utils.py
+++ app/ml/utils/db_utils.py
@@ -4,16 +4,42 @@ import pymysql.cursors
 from sqlalchemy import create_engine
 
 
+def get_parameters(param_key):
+    ssm = boto3.client("ssm", region_name="ap-northeast-1")
+    response = ssm.get_parameters(
+        Names=[
+            param_key,
+        ],
+        WithDecryption=True,
+    )
+    return response["Parameters"][0]["Value"]
+
+
 def connect():
-    config = {
-        "host": os.environ['MYSQL_HOST'],
-        "port": int(os.environ['MYSQL_PORT']),
-        "database": os.environ['MYSQL_DATABASE'],
-        "user": os.environ['MYSQL_USER'],
-        "password": os.environ['MYSQL_PASSWORD'],
-    }
+    if os.environ["APP_ENV"] == "prd":
+        import boto3
+
+        config = {
+            "host": get_parameters("MYSQL_HOST"),
+            "port": int(get_parameters("MYSQL_PORT")),
+            "database": get_parameters("MYSQL_DATABASE"),
+            "user": get_parameters("MYSQL_USER"),
+            "password": get_parameters("MYSQL_PASSWORD"),
+        }
+    else:
+        config = {
+            "host": os.environ["MYSQL_HOST"],
+            "port": int(os.environ["MYSQL_PORT"]),
+            "database": os.environ["MYSQL_DATABASE"],
+            "user": os.environ["MYSQL_USER"],
+            "password": os.environ["MYSQL_PASSWORD"],
+        }
     conn = pymysql.connect(**config)
-    engine = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8'.format(**config))
+    engine = create_engine(
+        "mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8".format(
+            **config
+        )
+    )
     return conn, engine
 
 
@@ -23,7 +49,7 @@ def execute(conn, sql):
             cur.execute(sql)
         conn.commit()
     except:
-        print('execution failed')
+        print("execution failed")
         conn.rollback()
 
 
@@ -36,8 +62,8 @@ def fetch_all(conn, sql):
 def load_from_csv(conn, table_name, file_path, columns):
     try:
         with conn.cursor() as cur:
-            with open(f'{file_path}', mode='r', encoding='utf-8') as f:
-                cur.copy_from(f, f'{table_name}', sep=',', columns=columns)
+            with open(f"{file_path}", mode="r", encoding="utf-8") as f:
+                cur.copy_from(f, f"{table_name}", sep=",", columns=columns)
         conn.commit()
     except:
         conn.rollback()
