25de89496505f48dd13a604ab0fa317f90bafcf6
NobuYoshi928 <softbank.nttdocomo@gmail.com>
2022-02-12 15:54:16 +0900

    キャッシュ削除

--- app/test.py
+++ app/test.py
@@ -1,16 +1,8 @@
-import app
+from time import sleep
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 if __name__ == "__main__":
-    app.predict(
-        n_clicks=1,
-        id=2732,
-        pregnancies=2,
-        glucose=108,
-        blood_pressure=80,
-        skinthickness=0,
-        insulin=0,
-        bmi=32.96959829,
-        diabetes_pedigree_function=0.241813673,
-        age=22,
-    )
-    app.register(n_clicks=1, index=2732, is_prediction_true="yes")
+    # browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome("/usr/bin/chromedriver")
